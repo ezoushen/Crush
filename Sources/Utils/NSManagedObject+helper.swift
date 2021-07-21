de7c900535d16be277367550e650318992748f49
ezoushen <ezoushen@gmail.com>
2021-07-22 01:07:33 +0800

    fix: Fix compiler error

--- Sources/Utils/NSManagedObject+helper.swift
+++ Sources/Utils/NSManagedObject+helper.swift
@@ -12,57 +12,8 @@ extension NSManagedObjectContext {
     func receive<T: NSManagedObject>(runtimeObject: T) -> T {
         let result = object(with: runtimeObject.objectID) as! T
         if runtimeObject.isFault == false {
-            result.willAccessValue(forKey: T.entity().attributeKeys.first!)
+            result.willAccessValue(forKey: T.entity().attributesByName.first!.key)
         }
         return result
-    }
-}
-
-// MARK: - Value getter
-
-extension NSManagedObject {
-    class var entityName: String {
-        return entity().name ?? String(reflecting: self)
-    }
-}
-
-// MARK: - Clone self
-
-protocol NSManagedObjectCloner: NSManagedObject { }
-
-extension NSManagedObject: NSManagedObjectCloner { }
-
-extension NSManagedObjectCloner {
-    func cloned(in context: NSManagedObjectContext) -> Self {
-        let cloned = Self.init(entity: Self.entity(), insertInto: context)
-        
-        // Copy attributes
-        self.entity.attributesByName.keys.forEach {
-            let value = self.value(forKey: $0)
-            cloned.setValue(value, forKey: $0)
-        }
-        
-        //Loop through all relationships, and clone them.
-        let relationships = NSEntityDescription.entity(forEntityName: Self.entity().name ?? "",
-                                                       in: context)?.relationshipsByName
-        
-        func mappingRelationships(_ name: String, _ description: NSRelationshipDescription) {
-            if description.isToMany {
-                let sourceSet = mutableSetValue(forKey: name)
-                let clonedSet = cloned.mutableSetValue(forKey: name)
-                let enumerator = sourceSet.objectEnumerator()
-                
-                while let relatedObject = enumerator.nextObject() as? NSManagedObject {
-                    clonedSet.add(relatedObject)
-                }
-            } else if let value = self.value(forKey: name) as? NSManagedObject {
-                let target = context.object(with: value.objectID)
-                cloned.setValue(target, forKey: name)
-            }
-        }
-        
-        relationships?.forEach(mappingRelationships)
-        
-        return cloned
     }
 }
